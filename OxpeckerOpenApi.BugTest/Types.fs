18477a24998a896e9323555c008d1438dca40bba
Reuben J. Sonnenberg <RJSonnenberg@users.noreply.github.com>
2026-02-10 14:11:50 -0900

    Refactor PersonSearchRequest and Person types for improved readability; add OpenAPI specification for the API

--- OxpeckerOpenApi.BugTest/Types.fs
+++ OxpeckerOpenApi.BugTest/Types.fs
@@ -1,7 +1,17 @@
 module OxpeckerOpenApi.BugTest.Types
 
 [<CLIMutable>]
-type PersonSearchRequest = { Name: string option; Occupation: string option }
+type PersonSearchRequest =
+    {
+        Name: string option
+        Occupation: string option
+    }
 
 [<CLIMutable>]
-type Person = { Name: string; Age: int; Occupation: string option; YearsExperience: int option }
+type Person =
+    {
+        Name: string
+        Age: int
+        Occupation: string option
+        YearsExperience: int option
+    }
